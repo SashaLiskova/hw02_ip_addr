7ee06cbacc8424beb83ec6bdafca4e0e7ae5674b
Sasha Liskova <alexandrabrsk7ls@gmail.com>
2023-09-30 18:31:29 +0200

    run workflow

--- main.cpp
+++ main.cpp
@@ -1,21 +1,10 @@
 #include "src/Parser.h"
-#include <fstream>
 
 int main()
 {
     Parser parser{}; 
-    // std::ifstream input("ip_filter.tsv");
-
-    // if(!input.is_open())
-    // {
-    //     std::cerr<<"no file\n";
-    //     return 1;
-    // }
-
     auto ipStorage = parser.parseInput(std::cin);
-    
-    //input.close();
-    
+        
     ipStorage.sort();
     ipStorage.print();
     ipStorage.filter(1).print();
